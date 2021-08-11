11a38430eb057aba9a2ea9ae348fa104542aa712
Jean Wisotscki <jean1wisotscki@gmail.com>
2021-08-10 21:02:30 -0300

    projeto em andamento

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// variáveis
+// variáveis globais
 var altura
 var largura
+var vidas = 1
+var tempo = 10
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -9,11 +11,30 @@ function ajustaTamanhoPalcoJogo() {
 }
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function(){
+    tempo--
+    if(tempo < 0 ){
+        //vitória
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+    } else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+} ,1000)
+
 function posicaoRandomica() {
 
     // remove mosquito anterior (caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
+
+        //vidas
+        if(vidas > 3){
+            //game over
+        } else{
+            document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+            vidas++
+        }
     }
 
     // posição aleatória do mosquito
@@ -33,6 +54,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)    
 }
